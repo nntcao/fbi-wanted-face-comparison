3183d83f453271a99ea85e07d43e3ec5258363fd
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-02-27 14:59:43 -0600

    added Person/Aliases models and tests

--- wanted_api/models.py
+++ wanted_api/models.py
@@ -1,3 +1,29 @@
+from tkinter import CASCADE
 from django.db import models
+from pyparsing import empty
 
 # Create your models here.
+class Person(models.Model):
+    title = models.CharField(max_length=50)
+    front_image = models.ImageField(upload_to='photos/')
+    url = models.URLField()
+    reward = models.IntegerField(default=0, blank=True)
+    race = models.CharField(max_length=50, blank=True)
+    nationality = models.CharField(max_length=50, blank=True)
+    details = models.TextField(blank=True)
+    height = models.IntegerField(blank=True)
+    weight = models.IntegerField(blank=True)
+    age = models.IntegerField(blank=True)
+    hair_color = models.CharField(blank=True)
+    eye_color = models.CharField(blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Aliases(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    alias = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.person.title + ': ' + self.alias
--- wanted_api/tests.py
+++ wanted_api/tests.py
@@ -1,3 +1,49 @@
+from ast import alias
+from re import M
+from turtle import title
 from django.test import TestCase
+from wanted_api.models import Person, Aliases
 
 # Create your tests here.
+
+
+class PeopleTestCase(TestCase):
+    def setUp(self):
+        test_person = Person.objects.create(title="test",
+            url="https://www.zenonscraper.com",
+            reward=0,
+            race="blue",
+            nationality="globe",
+            details="This is a test case testing the functionality of the model.",
+            height=5,
+            weight=3,
+            age=12,
+            hair_color="green",
+            eye_color="gray",
+            description="An empty text string"
+        )
+        test_person.save()
+        
+        test_alias1 = Aliases.objects.create(
+            person=test_person,
+            alias="The One"
+        )
+        test_alias2 = Aliases.objects.create(
+            person=test_person,
+            alias="The Second"
+        )
+
+        test_alias1.save()
+        test_alias2.save()
+
+    def test_get_person(self):
+        self.assertEqual(person.race, "blue")
+        self.assertEqual(person.height, 5)
+
+    def test_get_aliases(self):
+        person = Person.objects.get(race="blue")
+        aliases = Aliases(person=person)
+        self.assertEqual(aliases[0], "The One")
+        self.assertEqual(aliases[1], "The Second")
+
+
