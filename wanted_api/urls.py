91f0ab42a80a4d542cdd4402bad9f31f7d0fbf7e
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-03-06 23:48:47 -0600

    Cleaned up imports from wanted_api/urls.py

--- wanted_api/urls.py
+++ wanted_api/urls.py
@@ -1,11 +1,7 @@
-import django
-from django.http import HttpResponse
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('/hello', views.index),
+    path('hello/', views.index),
 ]
