64e7807ac35e8a033b8cd4f63de3e2fbd2a7e084
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-03-06 23:50:53 -0600

    Modified wanted_api/models.py to match JSON API
    Changed height, weight, and age to be min/max values respectively in the Person model. Added a caution field and allowed for null values on most columns in Person.

--- wanted_api/models.py
+++ wanted_api/models.py
@@ -1,22 +1,24 @@
-from tkinter import CASCADE
 from django.db import models
-from pyparsing import empty
 
 # Create your models here.
 class Person(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     front_image = models.ImageField(upload_to='photos/')
     url = models.URLField()
-    reward = models.IntegerField(default=0, blank=True)
-    race = models.CharField(max_length=50, blank=True)
-    nationality = models.CharField(max_length=50, blank=True)
-    details = models.TextField(blank=True)
-    height = models.IntegerField(blank=True)
-    weight = models.IntegerField(blank=True)
-    age = models.IntegerField(blank=True)
-    hair_color = models.CharField(blank=True)
-    eye_color = models.CharField(blank=True)
-    description = models.TextField(blank=True)
+    reward = models.IntegerField(default=0, null=True, blank=True)
+    race = models.CharField(max_length=50, null=True, blank=True)
+    nationality = models.CharField(max_length=50, null=True, blank=True)
+    details = models.TextField(null=True, blank=True)
+    height_min = models.IntegerField(default=0, null=True, blank=True)
+    height_max = models.IntegerField(default=0, null=True, blank=True)
+    weight_min = models.IntegerField(default=0, null=True, blank=True)
+    weight_max = models.IntegerField(default=0, null=True, blank=True)
+    age_min = models.IntegerField(default=0, null=True, blank=True)
+    age_max = models.IntegerField(default=0, null=True, blank=True)
+    hair_color = models.CharField(max_length=50, null=True, blank=True)
+    eye_color = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    caution = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
