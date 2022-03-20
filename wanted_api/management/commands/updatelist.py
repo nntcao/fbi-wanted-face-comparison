d23428d55bef3d3974aaea1b01ce4a401e634cd5
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-03-20 17:19:11 -0500

    Housekeeping debug

--- wanted_api/management/commands/updatelist.py
+++ wanted_api/management/commands/updatelist.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from wanted_api.external_api_service import get_wanted_list
 from wanted_api.models import Person
 
@@ -6,6 +6,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         wanted_list = get_wanted_list()
         persons = [Person.create_from_json(json) for json in wanted_list]
-        filtered_persons = list(filter(None, persons))
-        for person in filtered_persons:
-            print(person)
+        # filtered_persons = list(filter(None, persons))
+        # for person in filtered_persons:
+        #     print(person)
--- wanted_api/views.py
+++ wanted_api/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.conf import settings
+from wanted_api.ml_service import compare_all_persons
 
 # Create your views here.
 def index(request):
@@ -9,3 +9,6 @@ def index(request):
 def photo(request, id):
     with open(f'{settings.BASE_DIR}/photos/{id}', "rb") as f:
         return HttpResponse(f.read(), content_type="image/jpeg")
+
+# def find_doppel(request, id):
+    # compare_all_persons()
